0679529b30dc92562a458ff1ce440e3e2e49f8a8
yannis <thimotheeleclerc@gmail.com>
2017-10-19 13:02:14 +0200

    ajout interface graphique

--- prog.py
+++ prog.py
@@ -1,55 +1,67 @@
-import networkx as nx
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QInputDialog, QLineEdit
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtWidgets , QtCore, QtGui
+from PyQt5.QtWidgets import QGraphicsScene, QApplication, QFileDialog, QMainWindow
+from PyQt5.QtGui import QPixmap
 
-"""
-app = QApplication(sys.argv)
-
-w = QWidget()
-
-w.show()
-
-sys.exit(app.exec_())"""
-
-G=nx.read_graphml("data\GoT_S01E01_000.graphml")
-
-
-class App(QWidget):
+class Ui_MainWindow(QMainWindow):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(900,800)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(10,10,556,392))
+        self.graphicsView.setObjectName("graphicsView")
+        
  
-    def __init__(self):
-        super().__init__()
-        self.title = 'RC3'
-        self.setWindowIcon(QIcon('data\game.png'))
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.initUI()
- 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
         
         
-        button = QPushButton('PyQt5 button', self)
-        button.setToolTip('This is an example button')
-        button.move(150,400) 
-        button.clicked.connect(self.on_click)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0,0,673,125))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menufile")
+        MainWindow.setMenuBar(self.menubar)
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
         
-        button2 = QPushButton('PyQt5 button', self)
-        button2.setToolTip('This is an example button')
-        button2.move(250,400) 
-        button2.clicked.connect(self.on_click)
+        self.actionOpen.triggered.connect(self.showDialog)
         
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
         
-        self.show()
-        pyqtSlot()
-    def on_click(self):
-        print('PyQt5 button click')   
-    
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
+        self.actionExit.triggered.connect(QApplication.quit)
+        
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionExit)
+        self.menubar.addAction(self.menuFile.menuAction())
+        
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        
+    def showDialog(self):
+        fileName = QFileDialog.getOpenFileName(self, 'Open file')[0]
+        print(fileName)
+        self.scene = QGraphicsScene()
+        self.scene.addPixmap(QPixmap(fileName))
+        self.graphicsView.setScene(self.scene)
+        
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Mainwindow"))
+        self.menuFile.setTitle(_translate("MainWindow","F&ile"))
+        self.actionOpen.setText(_translate("MainWindow","Open"))
+        self.actionExit.setText(_translate("MainWindow","Exit"))
+        
+if __name__=='__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
+        
+        
+    
+        
