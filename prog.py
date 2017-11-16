83289191c14b15c9d195a9a845e11194b4f93037
yannis <thimotheeleclerc@gmail.com>
2017-11-16 11:12:02 +0100

    ajout d'éléments graphique comme des textsbox , menu déroulant

--- prog.py
+++ prog.py
@@ -1,11 +1,10 @@
 from PyQt5 import QtWidgets , QtCore, QtGui
-from PyQt5.QtWidgets import QGraphicsScene, QApplication, QFileDialog, QMainWindow,QLCDNumber, QSlider,QVBoxLayout, QPushButton,QLabel,QAction, QLineEdit, QMessageBox
+from PyQt5.QtWidgets import QGraphicsScene, QApplication, QFileDialog, QMainWindow,QLCDNumber, QSlider,QVBoxLayout, QPushButton,QLabel,QAction, QLineEdit, QMessageBox, QInputDialog, QComboBox, QSpinBox
 from PyQt5.QtGui import QPixmap
 
-class Ui_MainWindow(QMainWindow):
+class Ui_MainWindow():
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(900,800)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
@@ -29,25 +28,44 @@ class Ui_MainWindow(QMainWindow):
         self.menuFile.addAction(self.actionExit)
         self.menubar.addAction(self.menuFile.menuAction())
         self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)    
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+      
+        """
+        self.label = QLabel()
+        self.image = QPixmap("images/game.jpg")
+        self.label.setPixmap(self.image)"""
+        
                 
         # Create textbox
-        self.textbox = QLineEdit(self)
-        self.textbox.move(20, 20)
+        self.textbox = QLineEdit(MainWindow)
+        self.textbox.move(580, 80)
         self.textbox.resize(280,40)
  
         # Create a button in the window
-        self.button = QPushButton('Show text', self)
-        self.button.move(20,80)
+        self.button = QPushButton('Ok', MainWindow)
+        self.button.move(580,120)
  
         # connect button to function on_click
         self.button.clicked.connect(self.on_click)
-        self.show()
         
- 
+        #Getchoice
+        self.cb = QComboBox(MainWindow)
+        self.cb.move(580,50)
+        self.cb.addItem("C")
+        self.cb.addItem("C++")
+        self.cb.addItems(["Java", "C#", "Python"])
+        self.cb.currentIndexChanged.connect(self.selectionchange)
+        
+        #object presents the user with a integer
+        self.l1 = QLabel("current value:")
+        self.l1.move(650,50)
+        self.sp = QSpinBox(MainWindow)
+        self.sp.move(700,50)
+        self.sp.valueChanged.connect(self.valuechange)
+        
         
     def showDialog(self):
-        fileName = QFileDialog.getOpenFileName(self, 'Open file')[0]
+        fileName = QFileDialog.getOpenFileName(MainWindow, 'Open file')[0]
         print(fileName)
         self.scene = QGraphicsScene()
         self.scene.addPixmap(QPixmap(fileName))
@@ -63,8 +81,20 @@ class Ui_MainWindow(QMainWindow):
         
     def on_click(self):
         textboxValue = self.textbox.text()
-        QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
+        QMessageBox.question(MainWindow, 'ton texte', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
         self.textbox.setText("")
+        
+        
+    def selectionchange(self,i):
+        print ("Items in the list are :")
+        for count in range(self.cb.count()):
+            print (self.cb.itemText(count))
+        print ("Current index",i,"selection changed ",self.cb.currentText())
+        
+        
+    def valuechange(self):
+      self.l1.setText("current value:"+str(self.sp.value()))
+      print(self.sp.value())
         
         
 if __name__=='__main__':
@@ -74,5 +104,5 @@ if __name__=='__main__':
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     
-    MainWindow.show()
+    MainWindow.showMaximized()
     sys.exit(app.exec_())
